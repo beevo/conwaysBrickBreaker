f1a9f859929d7c32c243bb11ecc6b826475d0798
Bee Vo <mvo2@tigers.lsu.edu>
2014-01-26 01:48:15 -0600

    materials and scripts

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//+ f goes up, - f goes down
+		rigidbody.AddForce (100f	, 300f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if the input (in this case your arrow key) is left ( < 0)
+		//then move the paddle to the left
+		if( Input.GetAxis( "Horizontal") < 0 ){
+			Debug.Log("going left");
+			//move the paddle to the left
+			//-10f means to the left, you multiply it by deltaTime
+			//so it responds uniformly accross computer speeds.
+			transform.Translate(-10f * Time.deltaTime, 0, 0);
+		}
+		//same thing but to the right
+		if( Input.GetAxis( "Horizontal") > 0 ){
+			Debug.Log("going RIGHT");
+			transform.Translate(10f * Time.deltaTime, 0, 0);
+		}
+		//Debug.log() to get message to console!!
+	}
+}
